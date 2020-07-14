0a9ff2180bf87223b561c461e395f80ee2d7ba5e
Ruairi-osul <rorylikesmusic@gmail.com>
2020-07-14 15:14:46 +0100

    changed module for save_loco_video

--- mfreeze/video.py
+++ mfreeze/video.py
@@ -91,3 +91,70 @@ def save_freeze_video(
             break
     writer.release()
     cap.release()
+
+
+def save_loco_video(
+    source_video_path,
+    outfile_path,
+    xy=None,
+    start_frame=0,
+    stop_frame=None,
+    video_size=(640, 480),
+    crop_interactive=None,
+    crop_cmin=None,
+    crop_cmax=None,
+    crop_rmin=None,
+    crop_rmax=None,
+):
+    cap = cv2.VideoCapture(source_video_path)
+    stop_frame = (
+        int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) if stop_frame is None else stop_frame
+    )
+    num_frames = stop_frame - start_frame
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    _, frame_old = cap.read()
+    frame_old = cv2.cvtColor(frame_old, cv2.COLOR_BGR2GRAY)
+    frame_nrow, frame_ncol = frame_old.shape
+    using_crop, crop_settings = _parse_crop_settings(
+        frame_nrow,
+        frame_ncol,
+        crop_interactive,
+        crop_cmin,
+        crop_cmax,
+        crop_rmin,
+        crop_rmax,
+    )
+    if using_crop:
+        rmin, rmax, cmin, cmax = crop_settings
+        video_size = (int(cmax - cmin), int(rmax - rmin))
+    suffix = Path(outfile_path).suffix
+    if suffix == ".mp4":
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    elif suffix == ".avi":
+        fourcc = cv2.VideoWriter_fourcc(*"xvid")
+    else:
+        raise ValueError(f"Unknown file suffix for outfile_path: {suffix}")
+
+    writer = cv2.VideoWriter(outfile_path, fourcc, fps, video_size, False)
+
+    if xy is not None:
+        end_frame = stop_frame + 1
+        xy = xy[start_frame:end_frame]
+
+    cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+
+    for i in range(num_frames - 1):
+        has_frame, frame = cap.read()
+        if has_frame:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            if using_crop:
+                frame = _crop_frame(frame, *crop_settings)
+            if xy is not None:
+                pos = (int(xy[i, 1]), int(xy[i, 0]))
+                cv2.drawMarker(frame, position=pos, color=255)
+            writer.write(frame)
+        else:
+            warnings.warn(f"Error: Only wrote {i} of {num_frames}")
+            break
+    writer.release()
+    cap.release()
